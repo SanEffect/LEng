058a56699e219f7eb4fd7819d6340dcb86722f8a
SanEffect <Symbiot.dev@gmail.com>
2021-05-25 18:09:34 +0200

    separated models from entities

--- presentation/src/main/java/com/san/leng/ui/dictionary/DictionaryViewModel.kt
+++ presentation/src/main/java/com/san/leng/ui/dictionary/DictionaryViewModel.kt
@@ -1,6 +1,5 @@
 package com.san.leng.ui.dictionary
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
